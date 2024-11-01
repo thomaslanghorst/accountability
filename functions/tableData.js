195ec4d3a28d278e383f8ee92d0f74116d646efd
Thomas Langhorst <t.langhorst@gmx.de>
2024-11-01 08:23:22 +0100

    fix: removes unnecessary url parameter

--- functions/tableData.js
+++ functions/tableData.js
@@ -1,29 +1,25 @@
 export async function onRequest(context) {
-  const url = new URL(context.request.url);
-
-  const tableDataParam = url.searchParams.get('tableData');
-
-  if (tableDataParam && tableDataParam !== '' && tableDataParam === 'tableData') {
-    const data = await context.env.KV.get(tableDataParam);
+  try {
+    const data = await context.env.KV.get('tableData');
     return new Response(data);
+  } catch (error) {
+    return new Response('{"error": "not found"}', {
+      status: 404,
+    });
   }
-
-  return new Response('{"error": "not found"}', {
-    status: 404,
-  });
 }
 
 export async function onRequestPost(context) {
-    try {
-  
-      const formdata = await context.request.formData();
+  try {
 
-      const tableData = formdata.get('tableData');
-  
-      await context.env.KV.put('tableData', tableData);
-  
-      return new Response('{"success": true}');
-    } catch (error) {
-      return new Response(`error: ${error}`);
-    }
+    const formdata = await context.request.formData();
+
+    const tableData = formdata.get('tableData');
+
+    await context.env.KV.put('tableData', tableData);
+
+    return new Response('{"success": true}');
+  } catch (error) {
+    return new Response(`error: ${error}`);
   }
+}
