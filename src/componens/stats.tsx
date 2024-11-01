efff9a21ab4d9cd566592d675b36ab4ca80249e4
Thomas Langhorst <t.langhorst@gmx.de>
2024-11-01 08:37:58 +0100

    fix: swap written words and hours worked order to match table + fix Stat header

--- src/componens/stats.tsx
+++ src/componens/stats.tsx
@@ -10,25 +10,6 @@ export const Stats: FC<StatsProps> = ({ totalExtraWords, totalMinsWorked }) => {
   return (
     <div className='flex justify-center m-8'>
       <div className='stats shadow'>
-        {/* total written words */}
-        <div className="stat">
-          <div className="stat-figure text-primary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-8 w-8 stroke-current">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-            </svg>
-          </div>
-          <div className="stat-title text-primary">Total Written Words</div>
-          <div className="stat-value text-primary">{totalExtraWords}</div>
-          {/* <div className="stat-desc">21% more than last month</div> */}
-        </div>
         {/* total worktime */}
         <div className="stat">
           <div className="stat-figure text-secondary">
@@ -46,6 +27,25 @@ export const Stats: FC<StatsProps> = ({ totalExtraWords, totalMinsWorked }) => {
           </div>
           <div className="stat-title text-secondary">Total Time Worked</div>
           <div className="stat-value text-secondary">{durationString(`${totalMinsWorked}`)}</div>
+          {/* <div className="stat-desc">21% more than last month</div> */}
+        </div>
+         {/* total written words */}
+         <div className="stat">
+          <div className="stat-figure text-primary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-8 w-8 stroke-current">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+            </svg>
+          </div>
+          <div className="stat-title text-primary">Total Extra Words</div>
+          <div className="stat-value text-primary">{totalExtraWords}</div>
           {/* <div className="stat-desc">21% more than last month</div> */}
         </div>
       </div>
